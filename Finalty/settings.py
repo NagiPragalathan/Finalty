3cf6a4af6d5006c3986fe8a427d59613d915b4a8
NagiPragalathan <83810241+NagiPragalathan@users.noreply.github.com>
2023-12-12 00:27:47 +0530

    vercel host are updated

--- Finalty/settings.py
+++ Finalty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-od+82(4(e(i7t$9iwe%l(v^^t@xwfr!-28u73)sa0c(o+m(bf=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh','*']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = "Finalty.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
